0889faf6a8cb576081d45be2ae68a45dc636b904
magb7 <margaux.bedu@gmail.com>
2020-06-24 15:57:30 +0200

    import react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Join from "./components/Join";
 import Chat from "./components/Chat";
+
 const App = () => (
   <Router>
     <Route path="/" exact component={Join} />
