eab5c5336fd584ec1726f2f3040a8f0d2c2a84ae
magb7 <margaux.bedu@gmail.com>
2020-06-24 18:25:42 +0200

    implement first route for socket

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
-const socketio = require("socketio");
+const socketio = require("socket.io");
 const http = require("http");
-const PORT = process.env.PORT || 5000;
+
+const PORT = 5000;
+
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("We have a new connection");
+  socket.on("disconnect", () => {
+    console.log("User had left.");
+  });
+});
+
+app.use(router);
+
 server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
