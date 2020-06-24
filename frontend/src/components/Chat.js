793d282e55e07e01db1094867a2812123a123b6d
magb7 <margaux.bedu@gmail.com>
2020-06-24 15:49:01 +0200

    create components folder and add Chat and Join components

---
+++ frontend/src/components/Chat.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Chat = () => {
+  return (
+    <div>
+      <h1>Chat</h1>
+    </div>
+  );
+};
+export default Chat;
---
+++ frontend/src/components/Join.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Join = () => {
+  return (
+    <div>
+      <h1>Join</h1>
+    </div>
+  );
+};
+export default Join;
