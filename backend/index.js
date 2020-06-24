6a9159725664076dd95ca950d6a3452f002dcb7a
magb7 <margaux.bedu@gmail.com>
2020-06-24 19:25:19 +0200

    implement function to handle users and messages

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const { addUser, removeUser, getUser, getUserInRoom } = require("./users.js");
+
 const PORT = 5000;
 
 const router = require("./router");
@@ -12,6 +14,34 @@ const io = socketio(server);
 
 io.on("connection", (socket) => {
   console.log("We have a new connection");
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name},Welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined.` });
+
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
   socket.on("disconnect", () => {
     console.log("User had left.");
   });
