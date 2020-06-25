6dd9cc55decd5d6df01754b10b8d6808bc4025fe
magb7 <margaux.bedu@gmail.com>
2020-06-25 22:54:32 +0200

    resolve route problem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,38 +2,11 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./components/Chat";
 import PracticianPage from "./components/PracticianPage";
-import PatientPage from "./components/PatientPage";
 import SignIn from "./components/Join";
 import Login from "./components/Login";
 import Patients from "./Patients";
 import PatientsContext from "./contexts/patients-context";
 import "./App.css";
-
-const routes = [
-  {
-    path: "/patient",
-    component: PatientPage,
-  },
-  {
-    path: "/practician",
-    component: PracticianPage,
-  },
-];
-
-function RouteWithSubRoutes(route) {
-  return (
-    <>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Login} />
-          <Route path="/patient" exact component={SignIn} />
-          <Route path="/practician" exact component={PracticianPage} />
-          <Route path="/chat/:type/:name/:room" component={Chat} />
-        </Switch>
-      </Router>
-    </>
-  );
-}
 
 const App = () => {
   const [patients, setPatients] = useState([...Patients]);
@@ -43,11 +16,11 @@ const App = () => {
   return (
     <PatientsContext.Provider value={value}>
       <Router>
-        <Route path="/" exact component={Login} />
         <Switch>
-          {routes.map((route, i) => (
-            <RouteWithSubRoutes key={i} {...route} />
-          ))}
+          <Route path="/" exact component={Login} />
+          <Route path="/patient" exact component={SignIn} />
+          <Route path="/practician" exact component={PracticianPage} />
+          <Route path="/chat/:type/:name/:room" component={Chat} />
         </Switch>
       </Router>
     </PatientsContext.Provider>
