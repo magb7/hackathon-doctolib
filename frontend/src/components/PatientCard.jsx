13aeaf1eb9194a88438a8e09a43add3ad4a64158
magb7 <margaux.bedu@gmail.com>
2020-06-25 19:22:59 +0200

    add new image for false at risk patient and trying to implent toggle function

--- frontend/src/components/PatientCard.jsx
+++ frontend/src/components/PatientCard.jsx
@@ -3,9 +3,22 @@ import { Link } from "react-router-dom";
 import PatientsContext from "../contexts/patients-context";
 
 import "./styles/PatientCard.css";
+import Patients from "../Patients";
 
 const PatientCard = () => {
-  const { patients } = useContext(PatientsContext);
+  const { patients, setPatients } = useContext(PatientsContext);
+
+  const toggleAtRisk = (id) => {
+    console.log(id);
+    const togglePatients = patients.map((patient) => {
+      if (patient.id === id) {
+        console.log(patient.id);
+        console.log(patient.atRisk);
+      }
+    });
+    console.log(togglePatients);
+  };
+
   return (
     <>
       {patients.map((patient, index) => {
@@ -17,18 +30,48 @@ const PatientCard = () => {
                   {patient.firstname} {patient.lastname}
                 </p>
               </div>
-              <div className="card_icons">
+              <div
+                onClick={() => {
+                  console.log("toggle atRisk");
+                  toggleAtRisk(patient.id);
+                }}
+                className="card_icons"
+              >
                 {patient.atRisk ? (
                   <img src="/icons/bookmark_orange.png" alt="bookmark" />
-                ) : null}
+                ) : (
+                  <img
+                    src="/icons/bookmark-gray2.png"
+                    alt="bookmark"
+                    className="card_icons_disabled"
+                  />
+                )}
               </div>
             </div>
             <div className="card_info">
               <div className="card_mail">
-                Information contact : {patient.email}
+                <ul>
+                  Information contact:
+                  <li>
+                    <img
+                      className="card_mail_img"
+                      src="/icons/mail-01.svg"
+                      alt="mail icon"
+                    />
+                    {patient.email}
+                  </li>
+                  <li>
+                    <img
+                      className="card_phone_img"
+                      src="/icons/phone-01.svg"
+                      alt="mail icon"
+                    />
+                    {patient.phone}
+                  </li>
+                </ul>
               </div>
               <div className="card_maj">
-                Data update {patient.lastAppointment}
+                Last appointment: {patient.lastAppointment}
               </div>
             </div>
             <Link
