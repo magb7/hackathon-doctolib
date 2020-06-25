027c898fcbc0d44cb72ff860c3af6ee95cb16eb1
magb7 <margaux.bedu@gmail.com>
2020-06-25 09:54:03 +0200

    enable text message

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -15,13 +15,13 @@ const Message = ({ message: { text, user }, name }) => {
     <div className="messageContainer justifyEnd">
       <p className="sentText pr-10">{trimmedName}</p>
       <div className="messageBox backgroundBlue">
-        {/* <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p> */}
+        <p className="messageText colorWhite">{text}</p>
       </div>
     </div>
   ) : (
     <div className="messageContainer justifyStart">
       <div className="messageBox backgroundLight">
-        {/* <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p> */}
+        <p className="messageText colorDark">{text}</p>
       </div>
       <p className="sentText pl-10 ">{user}</p>
     </div>
