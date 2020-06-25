f57cd93db4ee5a84047cc48db44540016e6a6c17
narudb <benjam.d@gmail.com>
2020-06-25 22:48:52 +0200

    add context in patientmodal

--- frontend/src/components/PatientModal.jsx
+++ frontend/src/components/PatientModal.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './styles/PatientModal.css';
+import PatientsContext from '../contexts/patients-context';
 
-export default ({ close }) => (
-  <div className="modal">
-    <a className="close" onClick={close}>
-      &times;
-    </a>
-    <div className="header">PATIENT INFORMATION</div>
-    <div className="content">
-      {' '}
-      <p>Firstname :</p>
-      <p>Lastname :</p>
-      <p>Mail :</p>
-      <p>Last appointment :</p>
-      <p>Other :</p>
+const test = ({ close }) => {
+  const { patients, setPatients } = useContext(PatientsContext);
+  return (
+    <div className="modal">
+      <a className="close" onClick={close}>
+        &times;
+      </a>
+      <div className="header">PATIENT INFORMATION</div>
+      <div className="content">
+        {' '}
+        <ul>
+          <li>Firstname :</li>
+          <li>Lastname :</li>
+          <li>Mail :</li>
+          <li>Phone :</li>
+          <li>Last appointment :</li>
+        </ul>
+      </div>
+      <button>MODIFIER</button>
     </div>
-    <button>MODIFIER</button>
-  </div>
-);
+  );
+};
+export default test;
