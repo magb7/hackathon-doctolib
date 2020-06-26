25fb4da2cd90e8118a3eb1facf1f1da8465c3de5
llaulier <lioraloche@yahoo.fr>
2020-06-26 08:41:28 +0200

    import useless in LoginForm

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
